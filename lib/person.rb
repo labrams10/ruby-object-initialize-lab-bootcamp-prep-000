35613fbeb555207efe7e6d8cc9701db099544f16
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-05-03 01:52:22 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,13 +5,9 @@ class Person
 end
 
 class Dog
-  # def initialize(name)
-  #   @name = name
-  # end
-  #
-  # def name=(name)
-  #   @name = name
-  # end
+  def initialize(name)
+    @name = name
+  end
 
   def breed=(breed)
     @breed = breed
